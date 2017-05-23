76d47c784afe2681d36ba4b4eae2812c640acbc3
yazan <yazan@iris.edu>
2017-05-23 13:02:52 -0700

    removed some files

--- src/main/java/edu/iris/dmc/timeseries/model/Segment.java
+++ src/main/java/edu/iris/dmc/timeseries/model/Segment.java
@@ -1,8 +1,6 @@
 package edu.iris.dmc.timeseries.model;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
-import java.util.LinkedList;
 import java.util.List;
 
 import edu.iris.dmc.seedcodec.Type;
